5ed434069b5aefb14b6f59ae96d2539947c62d3c
CaicerNotebook <CaicerNotebook@DESKTOP-8GLL1T7>
2019-06-05 22:03:56 -0300

    3.16

--- Estructuras_Control_Flujo/Operaciones/Program.vb
+++ Estructuras_Control_Flujo/Operaciones/Program.vb
@@ -9,66 +9,42 @@ Module Program
     End Enum
     Sub Main(args As String())
         Dim valor1, valor2, valor3, valor4 As Integer
-        Dim caso As Byte
+        Dim caso As OPERACION
         Console.WriteLine("ingresar un numero")
         valor1 = Console.ReadLine()
         Console.WriteLine("ingresar un numero")
         valor2 = Console.ReadLine()
-        Console.WriteLine("la operacion")
+        Console.WriteLine("la operacion 1-SUMA 2-RESTA 3-MULTIPLICACION 4-DIVISION")
         caso = Console.ReadLine()
         Console.WriteLine(Opera(caso, valor1, valor2))
         Console.WriteLine("ingresar un numero")
         valor3 = Console.ReadLine()
-        Console.WriteLine("la operacion")
+        Console.WriteLine("la operacion 1-SUMA 2-RESTA 3-MULTIPLICACION 4-DIVISION")
         caso = Console.ReadLine()
         Console.WriteLine(Opera(caso, valor1, valor2, valor3))
         Console.WriteLine("ingresar un numero")
         valor4 = Console.ReadLine()
-        Console.WriteLine("la operacion")
+        Console.WriteLine("la operacion 1-SUMA 2-RESTA 3-MULTIPLICACION 4-DIVISION")
         caso = Console.ReadLine()
         Console.WriteLine(Opera(caso, valor1, valor2, valor3, valor4))
     End Sub
 
-    Function Opera(caso As Byte, valor1 As Decimal, valor2 As Integer) As Decimal
+    Function Opera(caso As OPERACION, valor1 As Decimal, valor2 As Integer) As Decimal
         Select Case caso
-            Case 1
-                valor1 = valor1 + valor2
-            Case 2
-                valor1 = valor1 - valor2
-            Case 3
-                valor1 = valor1 * valor2
-            Case 4
-                valor1 = valor1 / valor2
-
+            Case OPERACION.suma
+                Return valor1 + valor2
+            Case OPERACION.resta
+                Return valor1 - valor2
+            Case OPERACION.multipli
+                Return valor1 * valor2
+            Case OPERACION.division
+                Return valor1 / valor2
         End Select
-        Return valor1
     End Function
-    Function Opera(caso As Byte, valor1 As Decimal, valor2 As Integer, valor3 As Integer) As Decimal
-        Select Case caso
-            Case 1
-                valor1 = valor1 + valor2 + valor3
-            Case 2
-                valor1 = valor1 - valor2 - valor3
-            Case 3
-                valor1 = valor1 * valor2 * valor3
-            Case 4
-                valor1 = valor1 / valor2 / valor3
-
-        End Select
-        Return valor1
+    Function Opera(caso As OPERACION, valor1 As Decimal, valor2 As Integer, valor3 As Integer) As Decimal
+        Return Opera(caso, valor1, Opera(caso, valor2, valor3))
     End Function
-    Function Opera(caso As Byte, valor1 As Decimal, valor2 As Integer, valor3 As Integer, valor4 As Integer) As Decimal
-        Select Case caso
-            Case 1
-                valor1 = valor1 + valor2 + valor3 + valor4
-            Case 2
-                valor1 = valor1 - valor2 - valor3 - valor4
-            Case 3
-                valor1 = valor1 * valor2 * valor3 * valor4
-            Case 4
-                valor1 = valor1 / valor2 / valor3 / valor4
-
-        End Select
-        Return valor1
+    Function Opera(caso As OPERACION, valor1 As Decimal, valor2 As Integer, valor3 As Integer, valor4 As Integer) As Decimal
+        Return Opera(caso, Opera(caso, valor1, valor2), Opera(caso, valor3, valor4))
     End Function
 End Module
